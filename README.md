edfa2d52cf105c53a4a403a00adefabe2b7ab5b2
ch5 <kholis13@gmail.com>
2016-08-30 12:36:37 +0700

    Blog lanjutan

---
+++ README.md
@@ -0,0 +1,14 @@
+# Django 1.9 lanjutan
+
+- username = admin
+- password = asdf1234
+
+# Fitur
+- markdown
+- Django crispy
+- comment / Fade togle 
+- thread
+- replay comment
+- Dynamic previw form data
+
+
---
+++ src/comments/views.py
@@ -0,0 +1,73 @@
+from django.contrib import messages
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponseRedirect, Http404, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from comments.forms import CommentForm
+from comments.models import Comment
+
+def comment_delete(request, id):
+    try:
+        obj = Comment.objects.get(id=id)
+    except:
+        raise Http404
+    if obj.user != request.user:
+        response = HttpResponse("You don't have permision")
+        response.status_code = 403
+        return response
+
+    if request.method == "POST":
+        parent_obj_url = obj.content_object.get_absolute_url()
+        obj.delete()
+        messages.success(request, " This has been delete")
+        return HttpResponseRedirect(parent_obj_url)
+
+    context = {
+        "object":obj,
+    }
+    return render(request, "comment/confirm_delete.html", context)
+
+def comment_thread(request, id):
+    try:
+        obj = Comment.objects.get(id=id)
+    except:
+        raise Http404
+
+    content_object = obj.content_object
+    content_id = obj.content_object.id
+
+    initial_data = {
+        "content_type":obj.content_type,
+        "object_id":obj.object_id,
+    }
+
+    form = CommentForm(request.POST or None, initial=initial_data)
+    if form.is_valid():
+        c_type = form.cleaned_data.get("content_type")
+        content_type = ContentType.objects.get(model=c_type)
+        obj_id = form.cleaned_data.get("object_id")
+        content_data = form.cleaned_data.get("content")
+        parent_obj = None
+
+        try:
+            parent_id = int(request.POST.get("parent_id"))
+        except:
+            parent_id = None
+        if parent_id:
+            parent_qs = Comment.objects.filter(id=parent_id)
+            if parent_qs.exists() and parent_qs.count() == 1:
+                parent_obj = parent_qs.first()
+
+        new_comment, created = Comment.objects.get_or_create(
+                                    user = request.user,
+                                    content_type = content_type,
+                                    object_id = obj_id,
+                                    content = content_data,
+                                    parent = parent_obj
+                                        )
+        return HttpResponseRedirect(new_comment.content_object.get_absolute_url())
+
+    context = {
+        "comment":obj,
+        "form":form,
+    }
+    return render(request,"comment/comment_thread.html", context)
